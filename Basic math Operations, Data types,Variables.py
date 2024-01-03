a5706b2395b7421182afec5dc7dc564fa13ce2e2
MyAnalysis101 <150912822+MyAnalysis101@users.noreply.github.com>
2024-01-03 03:35:33 +0100

    Add files via upload

---
+++ Basic math Operations, Data types,Variables.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # BASIC MATHS OPERATIONS
+
+# In[4]:
+
+
+10+5
+
+
+# In[5]:
+
+
+1000+8999
+
+
+# In[6]:
+
+
+10-5
+
+
+# In[7]:
+
+
+print(10-5)
+
+
+# In[8]:
+
+
+10*5
+
+
+# In[9]:
+
+
+10/5
+
+
+# In[10]:
+
+
+100//12
+
+
+# In[11]:
+
+
+10//3
+
+
+# In[12]:
+
+
+10**3
+
+
+# In[14]:
+
+
+2+3*5**2                  
+
+
+# In[15]:
+
+
+((2+3)*5)**2
+
+
+# In[16]:
+
+
+100%75
+
+
+# In[17]:
+
+
+10%4
+
+
+# In[18]:
+
+
+77%23
+
+
+# In[20]:
+
+
+round(876.474,1)
+
+
+# # DATA TYPES
+
+# In[ ]:
+
+
+1. Integer, represented as int
+
+
+# In[23]:
+
+
+x=12
+type(x)
+
+
+# In[ ]:
+
+
+2. Float
+
+
+# In[26]:
+
+
+y=15.7
+type(y)
+
+
+# In[27]:
+
+
+n=1/3
+type(n)
+
+
+# In[ ]:
+
+
+3. Booleans
+
+
+# In[28]:
+
+
+20>10
+
+
+# In[29]:
+
+
+10==3
+
+
+# In[ ]:
+
+
+4. Strings (str)
+
+
+# In[33]:
+
+
+x="Nigeria will be great again"
+type(x)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
